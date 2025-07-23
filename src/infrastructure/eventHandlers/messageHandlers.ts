b67ba991c585e873c5fde210f7853f8ed5733cc7
cclaurevju <Carlos.Claure@jala.university>
2025-07-23 08:27:51 -0400

    feat: create message handlers

--- src/infrastructure/eventHandlers/messageHandlers.ts
+++ src/infrastructure/eventHandlers/messageHandlers.ts
@@ -1,14 +1,46 @@
-import { Server, Socket } from "socket.io";
+import "reflect-metadata";
+import { container } from "tsyringe";
+import { Socket, Server } from "socket.io";
+import { ApiClient } from "../http/ApiClient";
 import MessageDto from "../../application/dto/MessageDTO";
+
+const apiClient = container.resolve(ApiClient);
 
 export function registerMessageHandlers(socket: Socket, io: Server): void {
   console.log("Registering message handlers");
-  socket.on("send-message", (data) => {
+  socket.on("send-message", (data, callback) => {
     console.log("send-message ", data);
-    const message = data.message as MessageDto;
-    socket.to(message.chatId).emit("receive-message", message);
+    const message = data.message;
+    apiClient
+      .post(`/chats/${message.chatId}/messages`, message)
+      .then((res) => res.data)
+      .then((message) => {
+        callback({ message });
+        console.log(`receive-message to room: ${message.chatId}`, message);
+
+        socket.to(message.chatId).emit("receive-message", { message });
+        socket.to(message.receiversIds).emit("notify", {
+          title: "New message",
+          code: "new-message",
+          data: { message },
+        });
+      });
+
     console.log(
       `Message sent from: ${message.senderId} to chat: ${message.chatId}`
     );
+  });
+
+  socket.on("update-messages-status", (data) => {
+    const chatId = data.chatId;
+    const userId = data.userId;
+    console.log("update-messages-status", data);
+
+    apiClient
+      .put(`/chats/${chatId}/users/${userId}/messages/status`, null)
+      .then((res) => {
+        console.log("update-messages-status", { chatId, userId });
+        io.to(chatId).emit("update-messages-status", { chatId, userId });
+      });
   });
 }
