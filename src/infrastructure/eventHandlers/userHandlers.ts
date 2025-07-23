2e5d061e34f0340f12edb7a17a6df10ae47e6147
cclaurevju <Carlos.Claure@jala.university>
2025-07-23 08:27:38 -0400

    feat: create user handlers

--- src/infrastructure/eventHandlers/userHandlers.ts
+++ src/infrastructure/eventHandlers/userHandlers.ts
@@ -1,41 +1,36 @@
+import "reflect-metadata";
+import { container } from "tsyringe";
 import { Socket, Server } from "socket.io";
+import { ApiClient } from "../http/ApiClient";
 
-// interface ConnectedUser {
-//   userId: string;
-//   socketId: string;
-//   status: 'online' | 'offline';
-// }
-
-// const connectedUsers: ConnectedUser[] = [];
-
-const connectedUsers = new Map<string, string>();
+const onlineUsers = new Map<string, string>();
+const apiClient = container.resolve(ApiClient);
 
 export function registerUserHandlers(socket: Socket, io: Server): void {
   console.log("Registering user handlers");
   socket.on("user-connected", (data) => {
     const userId = data.userId;
     console.log("User connected: ", socket.id, data.userId);
-    if (!connectedUsers.has(data.userId))
-      connectedUsers.set(data.userId, socket.id);
-    console.log("Connected users: ", connectedUsers);
+    if (!onlineUsers.has(data.userId)) onlineUsers.set(data.userId, socket.id);
+    console.log("Connected users: ", onlineUsers);
     socket.join(userId);
     console.log("User joined to personal room: ", userId);
 
-    const array = Array.from(connectedUsers.keys());
-    io.emit("users-connected", array);
+    const array = Array.from(onlineUsers.keys());
+    io.emit("online-users", { onlineUsers: array });
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected: ", socket.id);
     const userId = socket.data.userId;
-    for (const [id, sId] of connectedUsers.entries()) {
+    for (const [id, sId] of onlineUsers.entries()) {
       if (sId === socket.id) {
-        connectedUsers.delete(id);
+        onlineUsers.delete(id);
         break;
       }
     }
 
-    const array = Array.from(connectedUsers.keys());
-    io.emit("users-connected", array);
+    const array = Array.from(onlineUsers.keys());
+    io.emit("online-users", { onlineUsers: array });
   });
 }
